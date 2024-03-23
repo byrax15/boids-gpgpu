ca51a668ea627ff87eae7623109f2bf9ef5a18fa
byrax15 <byraxis@gmail.com>
2024-03-23 01:17:48 -0400

    weird hiccups at lat/long boundary when slerping

--- contexts.ixx
+++ contexts.ixx
@@ -2,12 +2,18 @@
 // Created by byrax on 2024-02-24.
 //
 module;
+#define GLFW_INCLUDE_NONE
 #include <glbinding/gl46core/gl.h>
 #include <glbinding/glbinding.h>
 
-#include <SFML/Window.hpp>
+#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 #include <format>
+#include <print>
 #include <stdexcept>
 export module contexts;
 
@@ -16,8 +22,16 @@ private:
     opengl();
 
 public:
-    opengl(opengl const&) = delete;
+    ~opengl()
+    {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
 
+    opengl(opengl const&) = delete;
     opengl& operator=(opengl const&) = delete;
 
     [[nodiscard]] static auto const& instance()
@@ -28,30 +42,32 @@ public:
 
     auto window_aspect() const
     {
-        const auto [w, h] = window.getSize();
+        int w, h;
+        glfwGetWindowSize(window, &w, &h);
         return static_cast<float>(w) / static_cast<float>(h);
     }
 
-    auto operator->() const { return &window; }
+    auto operator*() const { return window; }
 
 private:
-    mutable sf::Window window;
+    GLFWwindow* window;
 };
 
 opengl::opengl()
-    : window(sf::Window { sf::VideoMode(1920, 1080, 32), "Boids", sf::Style::Default, std::invoke([] {
-                             using ctx_attrib = sf::ContextSettings::Attribute;
-#ifdef NDEBUG
-                             const auto flags = ctx_attrib::Core;
-#else
-                             const auto flags = ctx_attrib::Core | ctx_attrib::Debug;
-#endif
-                             return sf::ContextSettings(24, 8, 2, 4, 6, flags);
-                         }) })
 {
-    window.setVerticalSyncEnabled(true);
-    window.setActive();
-    glbinding::initialize(sf::Context::getFunction);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(640, 480, "Boids GPGPU", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glbinding::initialize(glfwGetProcAddress);
+
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460 core");
 
 #ifndef NDEBUG
     gl::glEnable(gl::GL_DEBUG_OUTPUT);
@@ -59,16 +75,15 @@ opengl::opengl()
         [](gl::GLenum source, gl::GLenum type, gl::GLuint id, gl::GLenum severity, gl::GLsizei, const gl::GLchar* message, const void*) {
             if (type != gl::GLenum::GL_DEBUG_TYPE_ERROR)
                 return;
-            throw std::runtime_error(std::format(R"(GL ERROR::
+            std::print(R"(GL ERROR::
     source:     0x{:x}
     type:       0x{:x}
     id:         0x{:x}
     severity:   0x{:x}
     message:    {:s})",
-                (uint32_t)source, (uint32_t)type, (uint32_t)id, (uint32_t)severity, message));
+                (uint32_t)source, (uint32_t)type, (uint32_t)id, (uint32_t)severity, message);
+            std::terminate();
         },
         nullptr);
-
 #endif
 }
-
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#define GLFW_INCLUDE_NONE
 #include <glbinding/gl46core/gl.h>
+
+#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/random.hpp>
 
 #define GLM_ENABLE_EXPERIMENTAL
@@ -11,6 +20,8 @@
 #include <chrono>
 #include <exception>
 #include <iostream>
+#include <numbers>
+#include <print>
 #include <span>
 #include <thread>
 #include <vector>
@@ -35,18 +46,57 @@ constexpr std::array model_boid {
 
 constexpr vec3 scene_size { 5, 5, 5 };
 constexpr size_t nBoids = 20;
-
 constexpr std::array boids_attribute_formats {
     vertex_attrib_format { 0, 0, 1, i_color_t::length(), 0, gl::GL_FLOAT, gl::GL_FALSE },
     vertex_attrib_format { 1, 1, 0, Face::value_type::length(), 0, gl::GL_FLOAT, gl::GL_FALSE },
 };
 const auto& [format_color, format_boid] = boids_attribute_formats;
 
+struct sim_state {
+    bool pause {};
+    float lat = 0, lon = 0;
+    float cam_speed = 3.14;
+    float deltaTime = 0;
+} sim_state;
+
 int main()
 {
     using namespace gl;
     try {
         const auto& gl_window = opengl::instance();
+        std::tuple user_data_ptrs { &gl_window, &sim_state };
+        glfwSetWindowUserPointer(*gl_window, &user_data_ptrs);
+        glfwSetKeyCallback(*gl_window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+            const auto& [gl_win, sim] = *reinterpret_cast<decltype(&user_data_ptrs)>(glfwGetWindowUserPointer(window));
+            if (action == GLFW_PRESS) {
+                switch (key) {
+                case GLFW_KEY_ESCAPE:
+                    glfwSetWindowShouldClose(window, true);
+                    break;
+                case GLFW_KEY_P:
+                    sim->pause = !sim->pause;
+                    break;
+                }
+            }
+
+            switch (key) {
+            case GLFW_KEY_LEFT:
+                sim->lon -= sim->cam_speed;
+                break;
+            case GLFW_KEY_RIGHT:
+                sim->lon += sim->cam_speed;
+                break;
+            case GLFW_KEY_DOWN:
+                sim->lat -= sim->cam_speed;
+                break;
+            case GLFW_KEY_UP:
+                sim->lat += sim->cam_speed;
+                break;
+            }
+        });
+        glfwSetFramebufferSizeCallback(*gl_window, [](GLFWwindow* window, int width, int height) {
+            glViewport(0, 0, width, height);
+        });
 
         const auto boid_prog = shader_program {
             gl_window,
@@ -91,7 +141,8 @@ int main()
 
         const auto buf_velocities = vertex_buffer { gl_window, std::span { (glm::vec4*)nullptr, nBoids } };
         std::ranges::for_each(buf_velocities.map_buffer(), [](auto& v) {
-            v = glm::linearRand(.1f * vec4 { -1, -1, -1, 0 }, .1f * vec4 { 1, 1, 1, 0 });
+            v = glm::linearRand(.5f * vec4 { -1, -1, -1, 0 }, .1f * vec4 { 1, 1, 1, 0 });
+            // v = {};
         });
         glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, buf_velocities);
 
@@ -104,29 +155,33 @@ int main()
         const auto buf_scene_size = vertex_buffer { gl_window, std::span { &scene_size, 1 } };
         glBindBufferBase(GL_UNIFORM_BUFFER, 2, buf_scene_size);
 
-        // const auto buf_debug_color = vertex_buffer { gl_window, std::span((vec4*)nullptr, 1) };
-        // glBindBufferBase(GL_UNIFORM_BUFFER, 3, buf_debug_color);
-
         glClearColor(0, 0, 0, 1);
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_CULL_FACE);
         glPointSize(10.f);
         glLineWidth(5.f);
 
-        chr::duration<float> frame_start, frame_end = chr::steady_clock::now().time_since_epoch();
-        while (gl_window->isOpen()) {
-            frame_start = chr::steady_clock::now().time_since_epoch();
-            const auto deltaTime = (frame_start - frame_end).count();
-            glNamedBufferSubData(buf_time, 0, buf_time.size_bytes(), &deltaTime);
-            frame_end = frame_start;
+        chr::duration<float>
+            frame_start = chr::steady_clock::now().time_since_epoch(),
+            frame_end = chr::steady_clock::now().time_since_epoch();
+        while (!glfwWindowShouldClose(*gl_window)) {
+            frame_end = chr::steady_clock::now().time_since_epoch();
+            sim_state.deltaTime = (frame_end - frame_start).count();
+            frame_start = frame_end;
+            glNamedBufferSubData(buf_time, 0, buf_time.size_bytes(), &sim_state.deltaTime);
 
             using view_t = decltype(buf_camera)::buffer_t;
-            // const vec3 view_point = glm::rotate(glm::mat4 { 1 }, 4 * std::sinf(.2f * now), vec3 { 0.f, 1.f, 0.f })
-            //     * vec4(0, 8, 10, 1);
-            const vec3 view_point
-                = glm::rotate(glm::identity<glm::mat4>(), glm::radians(-30.f), vec3(1, 0, 0))
-                * glm::rotate(glm::identity<glm::mat4>(), glm::radians(15.f), vec3(0, 1, 0))
-                * vec4(0, 0, 20, 0);
+            // const vec3 view_point =
+            //     //glm::rotate(glm::mat4 { 1 }, 4 * std::sinf(.2f * frame_start.count()), vec3 { 0.f, 1.f, 0.f }) *
+            //     vec4(0, 0, 20, 1);
+
+            static auto last_rot = glm::identity<glm::quat>();
+            last_rot = glm::mix(last_rot, glm::angleAxis(sim_state.lon, vec3(0, 1, 0)) * glm::angleAxis(sim_state.lat, vec3(1, 0, 0)),  sim_state.deltaTime);
+            const vec3 view_point = last_rot * vec4(0, 0, 20, 0);
+            /*const vec3 view_point
+                = glm::angleAxis(sim_state.lon, vec3(0, 1, 0))
+                * glm::angleAxis(sim_state.lat, vec3(1, 0, 0))
+                * vec4(0, 0, 20, 0);*/
             const auto next_view = glm::lookAt(view_point, glm::vec3 { 0.f, 0.f, 0.f }, glm::vec3 { 0.f, 1.f, 0.f });
             glNamedBufferSubData(buf_camera, 0, sizeof(view_t), &next_view[0][0]);
 
@@ -136,8 +191,10 @@ int main()
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            glUseProgram(move_prog);
-            glDispatchCompute(buf_positions.size(), 1, 1);
+            if (!sim_state.pause) {
+                glUseProgram(move_prog);
+                glDispatchCompute(buf_positions.size(), 1, 1);
+            }
 
             glUseProgram(boid_prog);
             glBindVertexArray(vao_boids);
@@ -148,24 +205,18 @@ int main()
             glUseProgram(debug_velocities_prog);
             glDrawArraysInstanced(GL_LINES, 0, 2, buf_positions.size());
 
-            gl_window->display();
+            glfwPollEvents();
+            //{
+            //    ImGui_ImplOpenGL3_NewFrame();
+            //    ImGui_ImplGlfw_NewFrame();
+            //    ImGui::NewFrame();
 
-            sf::Event event;
-            while (gl_window->pollEvent(event)) {
-                switch (event.type) {
-                case sf::Event::Resized:
-                    const auto [w, h] = event.size;
-                    gl::glViewport(0, 0, w, h);
-                    break;
-                case sf::Event::KeyPressed:
-                    if (event.key.code == sf::Keyboard::Escape)
-                        gl_window->close();
-                    break;
-                default:
-                    break;
-                }
-            }
+            //    //ImGui::Text("FPS %f", 1.f / deltaTime);
 
+            //    ImGui::Render();
+            //    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            //}
+            glfwSwapBuffers(*gl_window);
         }
     } catch (std::exception const& e) {
         std::cout << e.what() << "\n";
